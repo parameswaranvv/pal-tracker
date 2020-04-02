adbc45e96a3c3a8a2514c53955e7703021337a97
Param <parameswaranvv@gmail.com>
2020-04-02 15:47:55 -0500

    Initial commit

---
+++ src/test/java/io/pivotal/pal/tracker/PalTrackerApplicationTest.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class PalTrackerApplicationTest {
+
+	@Test
+	void contextLoads(){
+
+	}
+
+}
